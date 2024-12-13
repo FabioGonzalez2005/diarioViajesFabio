376fe4a898300bfdb65718fd5a4f04a114e2e971
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-13 10:27:17 +0000

    Creamos función obtenerRegionesPorPais

--- app/src/main/java/com/example/fabioroommapvm/model/RegionDAO.kt
+++ app/src/main/java/com/example/fabioroommapvm/model/RegionDAO.kt
@@ -11,6 +11,10 @@ interface RegionDAO {
     @Insert
     suspend fun insertarRegion(region: Region)
 
-    @Query("SELECT * FROM REGIONES")
+    @Query("SELECT * FROM Regiones")
     fun obtenerTodasRegiones(): Flow<List<Region>>
+
+    // Metodo para obtener regiones por país
+    @Query("SELECT * FROM Regiones WHERE idPais = :idPais")
+    fun obtenerRegionesPorPais(idPais: Int): Flow<List<Region>>
 }
