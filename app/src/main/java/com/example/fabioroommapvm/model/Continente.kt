96581892e64e08c46541572695ca578a01fdce48
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-13 10:31:03 +0000

    Creamos el dataclass de Continente

--- app/src/main/java/com/example/fabioroommapvm/model/Continente.kt
+++ app/src/main/java/com/example/fabioroommapvm/model/Continente.kt
@@ -1,3 +1,11 @@
 package com.example.fabioroommapvm.model
 
-data class Continente()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Continentes")
+data class Continente(
+    // Asignamos la primary key
+    @PrimaryKey(autoGenerate = true) val idContinente: Int = 0,
+    val nombreContinente: String
+)
