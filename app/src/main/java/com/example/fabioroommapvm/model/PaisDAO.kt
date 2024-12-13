43de38f57662cc4ddbb831370000ca4381cfe10f
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-13 10:27:01 +0000

    Creamos el método Transaction

--- app/src/main/java/com/example/fabioroommapvm/model/PaisDAO.kt
+++ app/src/main/java/com/example/fabioroommapvm/model/PaisDAO.kt
@@ -3,6 +3,7 @@ package com.example.fabioroommapvm.model
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -12,4 +13,9 @@ interface PaisDAO {
 
     @Query("SELECT * FROM Paises")
     fun obtenerTodosPaises(): Flow<List<Pais>>
+
+    // Metodo para obtener un país con sus regiones
+    @Transaction
+    @Query("SELECT * FROM Paises WHERE idPais = :idPais")
+    fun obtenerPaisConRegiones(idPais: Int): Flow<RegionConPais>
 }
