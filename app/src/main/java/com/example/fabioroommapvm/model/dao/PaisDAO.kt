b23f9cd4a6ae0c72f45865b8453f26874437b2bb
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-14 14:47:05 +0100

    Arreglamos funciones en PaisDAO

--- app/src/main/java/com/example/fabioroommapvm/model/dao/PaisDAO.kt
+++ app/src/main/java/com/example/fabioroommapvm/model/dao/PaisDAO.kt
@@ -3,17 +3,17 @@ package com.example.fabioroommapvm.model.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Transaction
 import com.example.fabioroommapvm.model.dataclasses.Pais
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PaisDAO {
+
     @Insert
     suspend fun insertarPais(pais: Pais)
 
     @Query("SELECT * FROM Paises WHERE idContinente = :idContinente")
-    fun obtenerPaisesPorContinente(idContinente: Int): Flow<List<Pais>> // Obtener países por continente
+    fun obtenerPaisesPorContinente(idContinente: Int): Flow<List<Pais>>
 
     @Query("SELECT * FROM Paises")
     fun obtenerTodosPaises(): Flow<List<Pais>>
