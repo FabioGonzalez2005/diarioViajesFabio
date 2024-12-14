940ad395dc989ad0b8bbe56933b18af1cb3768de
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-14 14:44:06 +0100

    Convertimos Viaje en la tabla superior

--- app/src/main/java/com/example/fabioroommapvm/model/dataclasses/Viaje.kt
+++ app/src/main/java/com/example/fabioroommapvm/model/dataclasses/Viaje.kt
@@ -3,22 +3,10 @@ package com.example.fabioroommapvm.model.dataclasses
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.ForeignKey
 
-@Entity(
-    tableName = "Viajes",
-    foreignKeys = [
-        ForeignKey(
-            entity = Pais::class,
-            parentColumns = ["idPais"],
-            childColumns = ["idPais"],
-            onDelete = ForeignKey.CASCADE // Si se elimina un país, los viajes relacionados también se eliminarán
-        )
-    ]
-)
+@Entity(tableName = "Viajes")
 data class Viaje(
     @PrimaryKey(autoGenerate = true) val idViaje: Int = 0,
     @ColumnInfo(name = "nombreViaje") val nombreViaje: String,
-    @ColumnInfo(name = "fechaViaje") val fechaViaje: String,
-    @ColumnInfo(name = "idPais") val idPais: Int // Relación con el Pais
+    @ColumnInfo(name = "fechaViaje") val fechaViaje: String
 )
