c8ea1f3e61d18ed68dd5c6e01cdeb49212e9d4ed
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-12-14 14:44:55 +0100

    Añadimos relación en Continente

--- app/src/main/java/com/example/fabioroommapvm/model/dataclasses/Continente.kt
+++ app/src/main/java/com/example/fabioroommapvm/model/dataclasses/Continente.kt
@@ -1,10 +1,22 @@
 package com.example.fabioroommapvm.model.dataclasses
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Continentes")
+@Entity(
+    tableName = "Continentes",
+    foreignKeys = [
+        ForeignKey(
+            entity = Viaje::class,
+            parentColumns = ["idViaje"],
+            childColumns = ["idViaje"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Continente(
     @PrimaryKey(autoGenerate = true) val idContinente: Int = 0,
-    val nombreContinente: String
+    val nombreContinente: String,
+    val idViaje: Int
 )
